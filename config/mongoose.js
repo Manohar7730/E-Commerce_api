e6668d046c67cf0dd9cf4536535b2c2c81e0b3db
Manohar <manohar20century@gmail.com>
2023-05-24 17:35:09 +0530

    added dotenv and made database in mongodb atlas cloud

--- config/mongoose.js
+++ config/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 
 // Connect to the MongoDB database
-mongoose.connect('mongodb://127.0.0.1:27017/E-Commerce_Api_project');
+mongoose.connect(process.env.URL);
 
 // Get the default connection
 const db = mongoose.connection;
