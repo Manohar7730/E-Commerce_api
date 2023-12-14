7d96782764f1ba08e7f755a2f8c14d445b727ab4
Manohar <manohar20century@gmail.com>
2023-12-14 13:05:56 +0530

    Updated routes for products and deletion

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import the Express module
-const express = require('express');
+const express = require("express");
 
 // Set the port number
 const port = process.env.PORT || 8000;
@@ -7,27 +7,27 @@ const port = process.env.PORT || 8000;
 // Create an instance of the Express application
 const app = express();
 
-const env = require('dotenv').config();
+const env = require("dotenv").config();
 
 // middleware's for parsing data in database
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
 // Import the Mongoose module for database connection
-const db = require('./config/mongoose');
+const db = require("./config/mongoose");
 
 // Import the router for product-related routes
-const productRouter = require('./routers/product');
+const productRouter = require("./routers/product");
 
 // Use the product router for all routes starting with '/'
-app.use('/products', productRouter);
+app.use("/", productRouter);
 
 // Start the server and listen on the specified port
 app.listen(port, (err) => {
-    if (err) {
-        // Print an error message if the server fails to start
-        console.log(`Error in connecting to the server: ${err}`);
-    }
-    // Print a success message if the server starts successfully
-    console.log(`Server is up and running successfully on port: ${port}`);
+  if (err) {
+    // Print an error message if the server fails to start
+    console.log(`Error in connecting to the server: ${err}`);
+  }
+  // Print a success message if the server starts successfully
+  console.log(`Server is up and running successfully on port: ${port}`);
 });
--- routers/product.js
+++ routers/product.js
@@ -1,17 +1,17 @@
 // Import the Express module
-const express = require('express');
+const express = require("express");
 
 // Create a new router instance
 const router = express.Router();
 
 // Import the product controller
-const productController = require('../controllers/productController');
+const productController = require("../controllers/productController");
 
 // Define a route for handling GET and post requests
-router.get('/', productController.products);
-router.post('/create', productController.create);
-router.delete('/:id',productController.delete);
-router.post('/:id/update_quantity',productController.update);
+router.get("/", productController.products);
+router.post("/create", productController.create);
+router.delete("/delete/:id", productController.delete);
+router.post("/:id/update_quantity", productController.update);
 
 // Export the router
 module.exports = router;
