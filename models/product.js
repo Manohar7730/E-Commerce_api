0710f87753062e152eacc149638b0d9658050490
pediredla manohar <manohar20century@gamil.com>
2025-03-24 16:07:22 +0530

    Added dotenv config for environment variables

--- models/product.js
+++ models/product.js
@@ -1,20 +1,22 @@
+require("dotenv").config();
+
 // Import the Mongoose module
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Define the product schema
 const productSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    quantity: {
-        type: Number,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true,
+  },
+  quantity: {
+    type: Number,
+    required: true,
+  },
 });
 
 // Create the Product model using the product schema
-const Product = mongoose.model('product', productSchema);
+const Product = mongoose.model("product", productSchema);
 
 // Export the Product model
 module.exports = Product;
